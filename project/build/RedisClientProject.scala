535a02b73e18128361864fdab3f715c754ee6599
Debasish Ghosh <debasish@debasishg.(none)>
2010-07-15 17:15:22 +0530

    compiles in 2.8.0 final

--- project/build/RedisClientProject.scala
+++ project/build/RedisClientProject.scala
@@ -21,7 +21,9 @@ class RedisClientProject(info: ProjectInfo) extends DefaultProject(info)
       case "2.8.0.RC3" =>
         "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC2-SNAPSHOT" % "test"
       case "2.8.0.RC7" =>
-        "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC6-SNAPSHOT" % "test"
+        "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC7-SNAPSHOT" % "test"
+      case "2.8.0" =>
+        "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.final-SNAPSHOT" % "test"
     }
   val junit = "junit" % "junit" % "4.8.1"
 }
